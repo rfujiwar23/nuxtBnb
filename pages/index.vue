25d29e5f2d48e317d8c52d2cc22cb69191f750bc
Ryo Fujiwara <jjcproject@icloud.com>
2022-05-08 14:58:54 +0900

    back in a while

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-for="home in homes" :key="home.objectID">
+    <img :src='home.images[0]' style="width:200px"><br>
+    {{ home.title}}<br>
+    </div>
+  </div>
+</template>
+<script>
+import { once } from 'events'
+import homes from '../data/homes.json'
+
+export default {
+  data(){
+    return {
+      homes: homes.slice(0,3)
+    }
+  }
+}
+</script>
